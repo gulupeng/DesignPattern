2fc170619640b4f68936d96211afd363f8badae1
lpgu <gulupeng@evercreative.com.cn>
2020-04-20 15:17:37 +0800

    增加消费者模式

---
+++ design-pattern/consumer-model/src/test/java/com/lpgu/consumer/ConsumerModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpgu.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
