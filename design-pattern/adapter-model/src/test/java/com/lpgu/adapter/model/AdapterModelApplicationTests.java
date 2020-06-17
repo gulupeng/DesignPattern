0e77f92dba9b20505b7fc116850df4f465a24703
lpgujdrx <gulupeng>
2020-06-17 22:00:25 +0800

    适配器模式

---
+++ design-pattern/adapter-model/src/test/java/com/lpgu/adapter/model/AdapterModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lpgu.adapter.model;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdapterModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
